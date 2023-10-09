6ba59703d42df9118ba99c7d15688519a4ddbd19
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-10-09 09:14:42 -0500

    feat (helpers): :hammer: Added Helpers
    Added helpers for encryption,token

---
+++ backend/helpers/Token.js
@@ -0,0 +1,34 @@
+import { SignJWT, jwtVerify } from "jose";
+import { PRIVATE } from "../config/config.js";
+
+const encoder = new TextEncoder();
+
+export const createToken = async (id) => {
+  try {
+    const jwtConstructor = new SignJWT({
+      id,
+    });
+
+    const token = await jwtConstructor
+      .setProtectedHeader({
+        alg: "HS256",
+        typ: "JWT",
+      })
+      .setIssuedAt()
+      .setExpirationTime("1h")
+      .sign(encoder.encode(PRIVATE.KEY));
+
+    return token;
+  } catch (error) {
+    throw new Error(`Error al generar el token ${error.message}`);
+  }
+};
+
+export const verifyToken = async (token) => {
+  try {
+    const { payload } = await jwtVerify(token, encoder.encode(PRIVATE.KEY));
+    return payload;
+  } catch (error) {
+    throw new Error(`Token invalido ${error.message}`);
+  }
+};
