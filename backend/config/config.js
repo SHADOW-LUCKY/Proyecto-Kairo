25181b593fe85de7e654f51bb4aae4dc267fbbb6
Akio1102 <cristiandaviddiazaza@gmail.com>
2023-10-09 09:10:47 -0500

    feat (config): :wrench: Configuration JWT
    Configuration for JWT

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 export const ENVPORT = {
   PORT: process.env.PORT456,
@@ -11,8 +12,13 @@ export const MONGODB = {
   URL: process.env.MANGOS_URL,
 };
 
+export const PRIVATE = {
+  KEY: process.env.PK,
+};
+
 export const Global = (app) => {
   app.use(morgan("dev"));
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
 };
