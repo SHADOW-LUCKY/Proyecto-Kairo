f3ab429d0a86c3685cbccc08ab24cbf0f108b596
SHADOW-LUCKY <nicordonezausecha@gmail.com>
2023-10-10 12:59:32 -0500

    feat: :art: improving dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,17 +1,26 @@
 import React,{useState} from 'react';
 /* images */
 import kairo from '../assets/kairo.png'
+import {HelpCircle} from '@styled-icons/boxicons-solid/HelpCircle'
+import {BugReport} from '@styled-icons/material-sharp/BugReport'
+import {DeleteForever} from '@styled-icons/material/DeleteForever'
+import {Refresh} from '@styled-icons/evaicons-solid/Refresh'
+import {PlusCircleFill} from '@styled-icons/bootstrap/PlusCircleFill'
+import {Gear} from '@styled-icons/evil/Gear'
+import {BellRing} from '@styled-icons/boxicons-solid/BellRing'
 /* components */
 import DashboardMenuList from './DashboardMenuList';
 
+
+/* function */
 export default function Dashboard() {
     const [open, setOpen] = useState(false)
     return (
       <div className='w-full h-screen bg-white'>
-        <div className='bg-white py-3 fixed top-0 left-0 right-0 shadow-md'>
+        <div className='bg-white h-14  fixed top-0 left-0 right-0 shadow-xl'>
             <div className='flex justify-between items-center md:hidden'>
-                <button className='ml-4 ' onClick={() => setOpen(true)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-black w-6 h-6">
+                <button className='m-4' onClick={() => setOpen(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-black w-6 h-6 ">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                 </button>
@@ -20,20 +29,52 @@ export default function Dashboard() {
                 </div>
 
             </div>
-            <div className='hidden md:flex'>
-                <div className='flex w-8/12  justify-between first-letter:'>
-                    <div className='text-center text-black text-xl hover:bg-orange-400 cursor-pointer py-3 justify-self-center'>Añadir</div>
-                    <div className='text-center text-black text-xl hover:bg-orange-400 cursor-pointer py-3 justify-self-center'>Refrescar</div>
-                    <div className='text-center text-black text-xl hover:bg-orange-400 cursor-pointer py-3 justify-self-center'>Eliminar</div>
-                    <div className='h-8'>
-                        <img src={kairo} alt="..." className='h- my-auto' />
-                    </div>
-                    <div className='text-center text-black text-xl hover:bg-orange-400 cursor-pointer py-3 justify-self-center'>Reportar</div>
-                    <div className='text-center text-black text-xl hover:bg-orange-400 cursor-pointer py-3 justify-self-center'>Ayuda</div>
-                </div>
-                <div>
+            <div className='hidden md:flex md:justify-end'>
 
-                </div>
+              <div className='flex w-9/12 h-full justify-between '>
+                    <div className='text-center text-black text-xl cursor-pointer py-3 my-auto flex'>
+                      <PlusCircleFill size={25} color='#FF7221'/>
+                      <p className='ml-2'>Añadir</p>
+                    </div>
+                    <div className='text-center text-black text-xl cursor-pointer py-3 my-auto flex'>
+                      <Refresh size={25} color='#FF7221'/>
+                      <p className='ml-2'>Refrescar</p>
+                    </div>
+                    <div className='text-center text-black text-xl cursor-pointer py-3 my-auto flex'>
+                      <DeleteForever size={30} color='#FF7221'/>
+                      <p className='ml-2'>Eliminar</p>
+                    </div>
+                    <div className='h-full my-auto'>
+                      <img src={kairo} alt="..." className='w-9' />
+                    </div>
+                    <div className='text-center text-black text-xl cursor-pointer py-3 my-auto flex'>
+                      <BugReport size={32} color='#FF7221'/>
+                      <p className='ml-2'>Reportar</p>
+                    </div>
+                    <div className='text-center text-black text-xl cursor-pointer py-3 my-auto flex'>
+                      <HelpCircle size={32} color='#FF7221'/>
+                      <p className='ml-2'>Ayuda</p>
+                    </div>
+              </div>
+              <div className='flex w-2/12 h-14 justify-end '>
+                  <div className='mr-2 my-auto'>
+                    <Gear size={20} color='#FF7221'/>
+                  </div>
+                  <div className='mr-2 my-auto'>
+                    <BellRing size={20} color='#FF7221'/>
+                  </div>
+                  <div className='mr-5 my-auto'>
+                    
+                    <div className="popover">
+                      <label className="popover-trigger my-2 " tabIndex="0"><img src='https://media.licdn.com/dms/image/C4E03AQGO448nAOrvfw/profile-displayphoto-shrink_800_800/0/1516929468945?e=2147483647&v=beta&t=d5P5hoCOyufB3Qkq935Dn4jIIpyoMVX_FweqCM3erjE' alt="..." className='w-9 rounded-full' /></label>
+                      <div className="popover-content popover-bottom-left bg-slate-400" tabIndex="0">
+                        <div className="popover-arrow bg-slate-400 border"></div>
+                        <div className="p-4 text-sm text-black">And here's some amazing content. It's very engaging. Right?</div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            
             </div>
         
   
--- frontend/src/components/DashboardMenuList.jsx
+++ frontend/src/components/DashboardMenuList.jsx
@@ -4,10 +4,20 @@ export default function DashboardMenuList() {
     const opts = [
         'Añadir','Refrescar','Eliminar','Reportar','Ayuda'
     ]
+    const components = [
+        'PlusCircleFill',
+        'Refresh',
+        'DeleteForever',
+        'BugReport',
+        'HelpCircle'
+    ]
     
   return (
       opts.map((item, index) => {
-       return  <div key={index} className='text-center text-black text-xl hover:bg-orange-400 cursor-pointer py-3 mb-2'>{item}</div>
+       return  <div key={index} className='text-center text-black text-xl cursor-pointer py-3 my-auto flex'>
+        
+        {item}
+        </div>
       })
   )
 }
